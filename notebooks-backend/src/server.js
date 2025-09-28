c8cbb3ada67c9caa8b5d52b0f6c187a01e4e4de7
Miggy1206 <miguelsbarbosa123@gmail.com>
2025-09-28 18:09:15 +0100

    Docker compose setup for notebook service

--- notebooks-backend/src/server.js
+++ notebooks-backend/src/server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 
 const app = express();
 const port = process.env.PORT;
 app.get('/',(req,res) => res.json({ message: 'Hello from notebooks' }));
 
-app.listen(port, () => {
-    console.log(`Notebooks server listening on port ${port}`);
-});
+
+mongoose
+.connect(process.env.DB_URL)
+    .then(() => {
+        console.log('Connected to MongoDB. Starting server.');
+
+        app.listen(port, () => {
+            console.log(`Notebooks server listening on port ${port}`);
+        });
+
+
+    })
+    .catch((err) => {
+        console.error('Something went wrong!');
+        console.error(err);
+    });
