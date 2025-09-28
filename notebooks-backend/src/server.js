6762954594a1b4dda055c2ad0ab3e25ab74c7c50
Miggy1206 <miguelsbarbosa123@gmail.com>
2025-09-28 21:22:51 +0100

    Connecting reverse proxies to services by overriding network

--- notebooks-backend/src/server.js
+++ notebooks-backend/src/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT;
-app.get('/',(req,res) => res.json({ message: 'Hello from notebooks' }));
+app.get('/api/notebooks',(req,res) => res.json({ message: 'Hello from notebooks' }));
 
 
 mongoose
--- notes-backend/src/server.js
+++ notes-backend/src/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT;
-app.get('/',(req,res) => res.json({ message: 'Hello from notes!!!' }));
+app.get('/api/notes',(req,res) => res.json({ message: 'Hello from notes!!!' }));
 
 
 mongoose
