bab63796faf2b557b7ffe1ce826cc148b3adab88
Miggy1206 <miguelsbarbosa123@gmail.com>
2025-09-28 22:08:08 +0100

    GET by id, PUT by id and DELETE by id - notebooks

--- notebooks-backend/src/routes.js
+++ notebooks-backend/src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const notebookRouter = express.Router();
 const { Notebook } = require('./models');
+const mongoose = require('mongoose');
 // Create new notebooks: POST '/'
 notebookRouter.post('/', async (req, res) => {
     try {
@@ -32,12 +33,55 @@ notebookRouter.get('/', async (req, res) => {
 
 //Retrieve a single notebook: GET '/:id'
 notebookRouter.get('/:id', async (req, res) => {
+    try{
+        const { id } = req.params;
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(404).json({ error: 'Notebook not found'});
+        }
+        const notebook = await Notebook.findById(id);
+        if(!notebook){
+            return res.status(404).json({ error: 'Notebook not found'});
+        }
+        return res.status(200).json({data: notebook});
+
+    }catch(err){
+        res.status(500).json({error:err.message});
+    }
 });
 //Update a single notebook: PUT '/:id'
 notebookRouter.put('/:id', async (req, res) => {
+    try{
+        const { name, description} = req.body;
+        const { id } = req.params;
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(404).json({ error: 'Notebook not found'});
+        }
+        const notebook = await Notebook.findByIdAndUpdate(id, {name, description}, {new: true});
+        if(!notebook){
+            return res.status(404).json({ error: 'Notebook not found'});
+        }
+        return res.status(200).json({data: notebook});
+
+    }catch(err){
+        res.status(500).json({error:err.message});
+    }
 });
 //Delete a single notebook: DELETE '/:id'
 notebookRouter.delete('/:id', async (req, res) => {
+    try{
+        const { id } = req.params;
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(404).json({ error: 'Notebook not found'});
+        }
+        const notebook = await Notebook.findByIdAndDelete(id);
+        if(!notebook){
+            return res.status(404).json({ error: 'Notebook not found'});
+        }
+        return res.json({data: notebook});
+
+    }catch(err){
+        res.status(500).json({error:err.message});
+    }
 });
 
 
