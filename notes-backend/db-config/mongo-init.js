4b9631b56518eb53b2b9c6ab2a867c2fea6db625
Miggy1206 <miguelsbarbosa123@gmail.com>
2025-09-28 18:25:12 +0100

    Dockerizing notes service

---
+++ notes-backend/db-config/mongo-init.js
@@ -0,0 +1,23 @@
+const notesDbUser = process.env.NOTES_DB_USER;
+const notesDbPassword = process.env.NOTES_DB_PASSWORD;
+const notesDbName = process.env.NOTES_DB_NAME;
+
+
+console.log('Initialising : Notebooks DB User');
+
+db = db.getSiblingDB(notesDbName);
+
+db.createUser({
+    user: notesDbUser,
+    pwd: notesDbPassword,
+    roles: [
+        {
+            role: 'readWrite',
+            db: notesDbName
+        }
+    ]
+});
+
+
+
+
